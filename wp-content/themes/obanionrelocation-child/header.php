028460e732a72463db92c1566393d93323716107
kritsanasak kuntaros <roboconk@gmail.com>
2016-11-07 22:41:33 +0700

    test responsive

--- wp-content/themes/obanionrelocation-child/header.php
+++ wp-content/themes/obanionrelocation-child/header.php
@@ -2,10 +2,19 @@
 <html <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php bloginfo('name'); ?></title>
+	
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-    <?php wp_head(); ?>
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php
 		/* We add some JavaScript to pages with the comment form
 		* to support sites with threaded comments (when in use).
@@ -20,24 +29,22 @@
 		*/
 		wp_head();
 	?>
+	<!-- Bootstrap -->
+	<script type="text/javascript" src="<?=get_template_directory_uri(); ?>-child/js/jquery.min.js"></script>
+	<script type="text/javascript" src="<?=get_template_directory_uri(); ?>-child/js/bootstrap.min.js"></script>
+    <link href="<?=get_template_directory_uri(); ?>-child/css/bootstrap.min.css" rel="stylesheet">
   </head>
 
 <body <?php body_class(); ?>>
-	<div class="container" style="width: 786px; margin: 0 auto; background-color: #ffffff;">
+	<div id="wrap" class="container">
 
-		<!--  site-header -->
-		<header class="site-header">
-			<h1>
-				<a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
-			</h1>
-			<h3>
-				<?php bloginfo('description'); ?>
-				<?php if (is_page (14)) { ?>
-				- Thank you for viewing our work.
-				<?php } ?>
-			</h3>
-		</header>
+		<?php if(defined('HOME_PHOTO')): ?>
+		<!--<img src="<?php bloginfo("template_url"); ?>/images/<?php echo HOME_PHOTO; ?>" class="center header-logo" >-->
+		<?php else: ?>
+		<img src="<?php bloginfo("template_url"); ?>/images/template_header.png" class="center" >
+		<?php endif; ?>
 
+		<?php ?>
 		<div id="menu" class="center">
 			<ol>
 				<?php $activePage[MENU_OPTION] = " menu-extend"; ?>
@@ -63,7 +70,7 @@
 					<a href="/getting-here">Getting Here</a>
 				</li>
 				<li id="menu-f" class="menuItem<?php echo $activePage['f'];?>">
-					<a href="/about-todd">About</a>
+					<a href="/about-todd">About Todd</a>
 				</li>
 				<li id="menu-g" class="menuItem<?php echo $activePage['g'];?>">
 					<a href="/faqs">FAQ's</a>
@@ -73,3 +80,27 @@
 				</li>
 			</ol>
 		</div>
+		<?php ?>
+
+		<?php /* ?>
+		<nav class="navbar navbar-default">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-6" aria-expanded="true">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button> 
+					<a href="#" class="navbar-brand">Brand</a>
+				</div>
+				<div class="navbar-collapse collapse in" id="bs-example-navbar-collapse-6" aria-expanded="true">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#">Home</a></li>
+						<li><a href="#">Link</a></li>
+						<li><a href="#">Link</a></li>
+					</ul> 
+				</div>
+			</div>
+		</nav>
+		<?php */ ?>
